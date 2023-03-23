90013ccf1ded1d5b135d573f261db6db06e88e1a
Soo-Hyeong.Lee <122865192+soohyeo@users.noreply.github.com>
2023-03-23 20:51:30 +0900

    역삼각형 별 찍기 실행완료

--- Test2440.java
+++ Test2440.java
@@ -10,8 +10,9 @@ public class Test2440 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("정수를 입력하시오.");
-		int N = Integer.parseInt(br.readLine());
-			for(int i = N; i >= 1; i--) {
+		int n = Integer.parseInt(br.readLine());
+			for(int i = n; i >= 1; i--) {
+				System.out.print("\n");
 				for(int j = 1; j <= i; j++) {
 					System.out.print("*");
 			}
