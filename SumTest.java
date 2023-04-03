1f7efcaa24cf03b966905a8fa9ffe3ed191a6cf2
Cat Hanbit <hello.git.asdfgwww123@gmail.com>
2023-04-03 16:55:28 +0900

    덧셈 테스트

---
+++ SumTest.java
@@ -0,0 +1,16 @@
+package progremous;
+
+public class SumTest{
+	
+	public static void main(String[] args) {
+		int num1 = 500;
+		int num2 = 600;
+		int answer = -1;
+		answer = num1 + num2;
+		
+		System.out.println(answer);
+	}
+	
+	
+
+}
