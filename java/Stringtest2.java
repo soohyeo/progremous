a587e7d55c883549776f058046e353e11005fb73
Soo-Hyeong.Lee <122865192+soohyeo@users.noreply.github.com>
2023-04-26 18:03:47 +0900

    문자열 테스트

---
+++ java/Stringtest2.java
@@ -0,0 +1,12 @@
+package kr.co.ezenac.test01;
+
+import java.util.Scanner;
+
+public class Stringtest2 {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        String a = sc.next();
+	        System.out.println(a);
+	    }
+	
+}
