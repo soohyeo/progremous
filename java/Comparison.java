9134a6e0bd397bba4c8832313f5b046bf80d3140
Soo-Hyeong.Lee <122865192+soohyeo@users.noreply.github.com>
2023-05-07 10:16:45 +0900

    A B 비교 연산자 조건 사용하기

---
+++ java/Comparison.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Comparison {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        if(A>B) {
+            System.out.println(">");
+        }
+        if(A<B){
+            System.out.println("<");
+        }
+        if(A==B){
+            System.out.println("==");
+        }
+
+    }
+}
