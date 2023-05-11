4770a2f6a0aea0ed32f73691f0bdfb844e52d41f
Soo-Hyeong.Lee <122865192+soohyeo@users.noreply.github.com>
2023-05-11 16:00:30 +0900

    StringBuilder사용 1

---
+++ java/ForSum.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ForSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = A+B;
+            System.out.println(C);
+        }
+    }
+}
