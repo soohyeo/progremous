ebbe3c698da1ca4105038557ee92edccbe587f7d
Soo-Hyeong.Lee <122865192+soohyeo@users.noreply.github.com>
2023-05-29 23:29:04 +0900

    input output 문자열 시도중

---
+++ java/InOutput.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class InOutput {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        String str = "";
+        for(int i=0; i<T; i++)
+        System.out.println();
+    }
+}
