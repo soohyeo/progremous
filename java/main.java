433f659ab1e1dd379a7fb0755c3811e90229815b
Soo-Hyeong.Lee <122865192+soohyeo@users.noreply.github.com>
2023-06-20 20:06:34 +0900

    18108번 1998년생인 내가 태국에서는 2541년생?!
    문제
    ICPC Bangkok Regional에 참가하기 위해 수완나품 국제공항에 막 도착한 팀 레드시프트 일행은 눈을 믿을 수 없었다. 공항의 대형 스크린에 올해가 2562년이라고 적혀 있던 것이었다.
    
    불교 국가인 태국은 불멸기원(佛滅紀元), 즉 석가모니가 열반한 해를 기준으로 연도를 세는 불기를 사용한다. 반면, 우리나라는 서기 연도를 사용하고 있다. 불기 연도가 주어질 때 이를 서기 연도로 바꿔 주는 프로그램을 작성하시오.
    
    입력
    서기 연도를 알아보고 싶은 불기 연도 y가 주어진다. (1000 ≤ y ≤ 3000)
    
    출력
    불기 연도를 서기 연도로 변환한 결과를 출력한다.

---
+++ java/main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int B = T-543;
+        System.out.println(B);
+    }
+}
