7e8ad660f35656a1f86321e3994dd6b9473ac47d
Soo-Hyeong.Lee <122865192+soohyeo@users.noreply.github.com>
2023-07-06 21:35:17 +0900

    8393번 합 Bronze 5
    문제
    n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
    
    출력
    1부터 n까지 합을 출력한다.

--- java/Sum.java
+++ java/Sum.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Sum {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = A+B;
+        int N = Integer.parseInt(br.readLine());
 
-        System.out.println(C);
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
 
-
+        System.out.print(sum);
     }
 }
